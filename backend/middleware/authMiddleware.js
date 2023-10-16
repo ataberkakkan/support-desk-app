989c2807d8410c72e61a1dfbb98da199e15c0f40
ataberkakkan <akkanataberk@gmail.com>
2023-10-16 09:03:20 +0300

    Protected Routes

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const asynHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const protect = asynHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not Authrorized");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not Authrorized");
+  }
+});
+
+module.exports = { protect };
